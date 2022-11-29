f4fe0e3aeb9ab31e23bfaf7a03d6598772363bf3
belopot <belopot@protonmail.com>
2022-11-30 00:12:14 +0800

    remove

--- src/components/Labels/index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const H1 = styled.div`
-  font-size: 1em;
-  font-weight: 700;
-  color: #ffffff;
-  line-height: 1.2;
-  letter-spacing: 0.01em;
-`;
